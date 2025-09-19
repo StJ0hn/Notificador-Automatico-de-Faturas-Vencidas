b2d012cfbf70aff6472457ccdabc7e2cc04508a9
StJ0hn <jonhmiguel138@gmail.com>
2025-09-18 21:16:56 -0300

    Alterações no verificador no bloco que itera cada cliente e efetua o envio da notificação via e-mail

--- verificador.py
+++ verificador.py
@@ -20,25 +20,25 @@ for index, linha in faturas_vencidas.iterrows():
     vencimento = linha['data_vencimento']
     print(f"Cliente: {cliente}, E-mail: {email} - Cobrança no valor de R${valor:.2f} vencida em {vencimento.strftime('%d/%m/%Y')}")
 
-msg = EmailMessage()
-msg['Subject'] = f"Aviso de fatura Vencida - {cliente}"
-msg['From'] = MEU_EMAIL
-msg['To'] = email
+    msg = EmailMessage()
+    msg['Subject'] = f"Aviso de fatura Vencida - {cliente}"
+    msg['From'] = MEU_EMAIL
+    msg['To'] = email
 
-corpo_email = f"""
-<p>Prezado(a) Cliente: {cliente},<p>
-<p>Este é um aviso automático referente à sua fatura no valor de <strong>R${valor:,.2f}</strong>, que venceu em <strong>{vencimento.strftime('%d/%m/%Y')}</strong>.</p>
-<p>Por favor, realize a regularização o mais breve possível para evitar maiores transtornos.</p>
-<p>Em caso de dúvidas ou se o pagamento já foi efetuado, por favor, desconsidere este e-mail.</p>
-<p>Atenciosamente,</p>
-<p>Sistema de Cobranças Automático</p>
-"""
-msg.set_content(corpo_email, subtype='html')
+    corpo_email = f"""
+    <p>Prezado(a) Cliente: {cliente},<p>
+    <p>Este é um aviso automático referente à sua fatura no valor de <strong>R${valor:,.2f}</strong>, que venceu em <strong>{vencimento.strftime('%d/%m/%Y')}</strong>.</p>
+    <p>Por favor, realize a regularização o mais breve possível para evitar maiores transtornos.</p>
+    <p>Em caso de dúvidas ou se o pagamento já foi efetuado, por favor, desconsidere este e-mail.</p>
+    <p>Atenciosamente,</p>
+    <p>Sistema de Cobranças Automático</p>
+    """
+    msg.set_content(corpo_email, subtype='html')
 
-try:
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(MEU_EMAIL, MINHA_SENHA_APP)
-        smtp.send_message(msg)
-        print(f"-> E-mail de cobrança para {cliente} enviado com sucesso!")
-except Exception as e:
-    print(f"!! ERRO ao enviar e-mail para {cliente}: {e}")
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(MEU_EMAIL, MINHA_SENHA_APP)
+            smtp.send_message(msg)
+            print(f"-> E-mail de cobrança para {cliente} enviado com sucesso!")
+    except Exception as e:
+        print(f"!! ERRO ao enviar e-mail para {cliente}: {e}")
